d813ff036d53be9a40ec37c1b6e0ca51dd65ce27
Giordani da Silveira <dani.santossilveira@hotmail.com>
2025-02-27 19:42:54 -0300

    ajustes view noticia

--- resources/views/site/noticia/view.blade.php
+++ resources/views/site/noticia/view.blade.php
@@ -10,34 +10,33 @@ if(count($noticia->arquivos) != 0){
 
 @section('conteudo')
     {{ Breadcrumbs::render('noticiaView', $noticia) }}
-    <div class="container-fluid overflow-hidden py-5 px-lg-0">
-        <div class="container about py-5 px-lg-0">
-            <div class="row g-5 mx-lg-0">
-                <div class="col-lg-6 ps-lg-0 wow fadeInLeft" data-wow-delay="0.1s" style="min-height: 400px;">
-                    @if ($fotos)
-                        <div class="owl-carousel header-carousel position-relative mb-5">
-                            @foreach ($fotos as $arquivo)
-                                <div class="owl-carousel-item position-relative">
-                                    <img class="img-fluid" src="{{url('/')}}/storage/uploads/noticia/{{$arquivo->id}}/{{$arquivo->arquivo}}" alt="{{$arquivo->nome_original}}">
-                                </div>
-                            @endforeach
-                        </div>
+    <div class="container py-5 px-lg-0">
+        <div class="row g-5 mx-lg-0">
+            <div class="col-lg-12 about-text wow fadeInUp" data-wow-delay="0.3s">
+                <h1 class="mb-5">{{$noticia->titulo}}</h1>
+                <p>
+                    @if ($noticia->autor)
+                        <strong>Autor: </strong>{{$noticia->autor}}<br />
                     @endif
-                </div>
-                <div class="col-lg-6 about-text wow fadeInUp" data-wow-delay="0.3s">
-                    <h1 class="mb-5">{{$noticia->titulo}}</h1>
-                    <p>
-                        @if ($noticia->autor)
-                            <strong>Autor: </strong>{{$noticia->autor}}<br />
-                        @endif
-                        @if ($noticia->data_publicacao)
-                            <strong>Data de Publicação: </strong> {{$noticia->data_publicacao->format('d/m/Y')}}<br>
-                        @endif
-                    </p>
-                    @if ($noticia->descricao)
-                        <?=$noticia->descricao?>
+                    @if ($noticia->data_publicacao)
+                        <strong>Data de Publicação: </strong> {{$noticia->data_publicacao->format('d/m/Y')}}<br>
                     @endif
-                </div>
+                </p>
+                <hr><br>
+                @if ($noticia->descricao)
+                    <?=$noticia->descricao?>
+                @endif
+            </div>
+            <div class="col-lg-12 wow fadeInLeft" data-wow-delay="0.1s" style="min-height: 400px;">
+                @if ($fotos)
+                    <div class="owl-carousel header-carousel position-relative mb-5">
+                        @foreach ($fotos as $arquivo)
+                            <div class="owl-carousel-item position-relative">
+                                <img class="img-fluid" src="{{url('/')}}/storage/uploads/noticia/{{$arquivo->id}}/{{$arquivo->arquivo}}" alt="{{$arquivo->nome_original}}">
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
             </div>
         </div>
     </div>
